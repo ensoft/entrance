1c7eaabac2c8b84a40fa1f236e708250e2ff6cf8
Simon Chatterjee <code@chatts.net>
2019-09-17 11:33:28 +0100

    move to latest sanic

--- python/setup.py
+++ python/setup.py
@@ -22,18 +22,24 @@ extra_deps = []
 
 # The very latest version of sanic (19.6.3 onwards) require websockets 7.x, but before
 # that requires 6.x. Also, sanic dropped support for Python 3.5, leaving a long-term
-# support version at 18.12. So stick to the older websockets for now, and pin sanic.
+# support version at 18.12. So support either the oldest or newest supported package set.
 v = sys.version_info
 assert v.major == 3
 assert v.minor >= 5
-sanic_fix = 'sanic==18.12.0' if v.minor == 5 else 'sanic<=19.6.2'
-websockets_fix = 'websockets>=6.0,<7.0'
+if v.minor == 5:
+    # Ye Olde Python 3.5
+    sanic = 'sanic==18.12.0'
+    websockets = 'websockets>=6.0,<7.0'
+else:
+    # Python 3.6+
+    sanic = 'sanic>=19.6.3'
+    websockets = 'websockets>=7.0,<8.0'
 
 with open('README.md', 'r') as f:
     long_description = f.read()
 
 setup(name='entrance',
-      version='1.1.8',
+      version='1.1.9',
       author='Ensoft Ltd',
       description='Server framework for web apps',
       url='https://github.com/ensoft/entrance',
@@ -51,7 +57,7 @@ setup(name='entrance',
         'Programming Language :: Python :: 3.7'
       ],
 
-      install_requires=['pyyaml', sanic_fix, websockets_fix] + extra_deps,
+      install_requires=['pyyaml', sanic, websockets] + extra_deps,
 
       extras_require={
         'with-router-features': router_feature_deps
