3aa442e48561e5c1822a4c41e4ae0a0e812358f1
Simon Chatterjee <code@chatts.net>
2020-07-06 13:27:19 +0100

    Bump python version

--- python/setup.py
+++ python/setup.py
@@ -35,7 +35,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="entrance",
-    version="1.1.13",
+    version="1.1.14",
     author="Ensoft Ltd",
     description="Server framework for web apps",
     url="https://github.com/ensoft/entrance",
